778582be9622bb19ab025ba66cee140d0be9cdef
Isaac <nisaacdz@gmail.com>
2025-12-02 17:09:47 +0000

    Implement day1 part2 challenge

---
+++ day01-B/cpp/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+
+const char ENDL = '\n';
+const ll MOD = 1e9 + 7;
+
+int main()
+{
+    cout << "STARTING" << ENDL;
+    std::ifstream in("../input.txt");
+    ll steps_thru_zero = 0;
+    ll current_stop = 50;
+    string line;
+    while (std::getline(in, line))
+    {
+        ll steps = stoi(line.substr(1));
+
+        steps_thru_zero += steps / 100;
+        steps = steps % 100;
+
+        if (line[0] == 'L') {
+            steps_thru_zero += current_stop - steps < 0;
+            current_stop = ((current_stop - steps) % 100 + 100) % 100;
+        } else {
+            steps_thru_zero += current_stop + steps > 99;
+            current_stop = (current_stop + steps) % 100;
+        }
+    }
+    cout << steps_thru_zero << ENDL;
+    return 0;
+}
