3cc37ce4f4ec27873ecd6329a7a30dd8fc66ff83
Isaac <nisaacdz@gmail.com>
2025-12-08 16:30:43 +0000

    Solve day 7 problem part A

--- day07-A/rust/src/main.rs
+++ day07-A/rust/src/main.rs
@@ -1,46 +1,51 @@
-use std::{fs::File, io::{BufRead, BufReader, Result}};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Result},
+};
 
 fn main() -> Result<()> {
     let file = File::open("../input.txt")?;
     let reader = BufReader::new(file);
-    let mut last_line = None::<String>;
     let mut mat = Vec::new();
     for line in reader.lines() {
         let line = line?;
-        if let Some(prev) = &last_line {
-            let row = prev.split_whitespace().map(|s| s.trim().parse::<u32>().unwrap()).collect::<Vec<_>>();
-            mat.push(row);
-        }
-
-        last_line = Some(line);
+        mat.push(line.into_bytes());
     }
 
-    let operations = last_line.unwrap().split_whitespace().map(|v| v.as_bytes()[0]).collect::<Vec<_>>();
-
-    let result = solve(mat, operations);
+    let result = solve(mat);
     println!("Result: {}", result);
 
     Ok(())
 }
 
-fn operate(value: u32, cur: &mut u64, op: u8) {
-    match op {
-        b'+' => *cur += value as u64,
-        b'*' => *cur *= value as u64,
-        _ => panic!("Unknown operation"),
-    }
-}
-
-fn solve(matrix: Vec<Vec<u32>>, operations: Vec<u8>) -> u64 {
+fn solve(mut mat: Vec<Vec<u8>>) -> u64 {
     let mut result = 0;
 
-    for j in 0..operations.len() {
-        let mut cur = if operations[j] == b'+' { 0 } else { 1 };
-        for i in 0..matrix.len() {
-            operate(matrix[i][j], &mut cur, operations[j]);
+    let idx = mat[0].iter().position(|&c| c == b'S').unwrap();
+    mat[0][idx] = b'|';
+
+    for i in 1..mat.len() {
+        for j in 0..mat[i].len() {
+            if mat[i][j] == b'.' {
+                if mat[i - 1][j] == b'|' {
+                    mat[i][j] = b'|';
+                }
+            } else if mat[i][j] == b'^' {
+                if mat[i - 1][j] == b'|' {
+                    result += 1;
+                    if j > 0 && mat[i][j - 1] == b'.' {
+                        mat[i][j - 1] = b'|'
+                    };
+                    if i + 1 < mat[0].len() && mat[i][j + 1] == b'.' {
+                        mat[i][j + 1] = b'|'
+                    };
+                }
+            }
         }
-        result += cur;
     }
+
+    //let mat = mat.into_iter().map(|v| String::from_utf8(v).unwrap()).collect::<Vec<_>>();
+    //println!("{:#?}", mat);
 
     result
 }
