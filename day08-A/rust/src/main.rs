0fec8ccc3b54dd3ef9fce2bd9eeabc30cedf4c66
Isaac <nisaacdz@gmail.com>
2025-12-08 22:07:57 +0000

    Setup for day 8 problems

---
+++ day08-A/rust/src/main.rs
@@ -0,0 +1,51 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Result},
+};
+
+fn main() -> Result<()> {
+    let file = File::open("../input.txt")?;
+    let reader = BufReader::new(file);
+    let mut mat = Vec::new();
+    for line in reader.lines() {
+        let line = line?;
+        mat.push(line.into_bytes());
+    }
+
+    let result = solve(mat);
+    println!("Result: {}", result);
+
+    Ok(())
+}
+
+fn solve(mut mat: Vec<Vec<u8>>) -> u64 {
+    let mut result = 0;
+
+    let idx = mat[0].iter().position(|&c| c == b'S').unwrap();
+    mat[0][idx] = b'|';
+
+    for i in 1..mat.len() {
+        for j in 0..mat[i].len() {
+            if mat[i][j] == b'.' {
+                if mat[i - 1][j] == b'|' {
+                    mat[i][j] = b'|';
+                }
+            } else if mat[i][j] == b'^' {
+                if mat[i - 1][j] == b'|' {
+                    result += 1;
+                    if j > 0 && mat[i][j - 1] == b'.' {
+                        mat[i][j - 1] = b'|';
+                    };
+                    if i + 1 < mat[0].len() && mat[i][j + 1] == b'.' {
+                        mat[i][j + 1] = b'|';
+                    };
+                }
+            }
+        }
+    }
+
+    //let mat = mat.into_iter().map(|v| String::from_utf8(v).unwrap()).collect::<Vec<_>>();
+    //println!("{:#?}", mat);
+
+    result
+}
---
+++ day08-B/rust/src/main.rs
@@ -0,0 +1,45 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Result},
+};
+
+fn main() -> Result<()> {
+    let file = File::open("../input2.txt")?;
+    let reader = BufReader::new(file);
+    let mut mat = Vec::new();
+    for line in reader.lines() {
+        let line = line?;
+        mat.push(line.into_bytes());
+    }
+
+    let result = solve(mat);
+    println!("Result: {}", result);
+
+    Ok(())
+}
+
+fn solve(mut mat: Vec<Vec<u8>>) -> u64 {
+    let (n, m) = (mat.len(), mat[0].len());
+    let mut dp = vec![vec![0; mat[0].len()]; mat.len()];
+
+    let idx = mat[0].iter().position(|&c| c == b'S').unwrap();
+    mat[0][idx] = b'|';
+
+    dp[n - 1].iter_mut().for_each(|v| *v = 1);
+
+    for i in (0..(n - 1)).rev() {
+        for j in 0..m {
+            if mat[i][j] == b'^' {
+                dp[i][j] += if j > 0 { dp[i + 1][j - 1] } else { 0 };
+                dp[i][j] += if j + 1 < m { dp[i + 1][j + 1] } else { 0 };
+            } else {
+                dp[i][j] += dp[i + 1][j];
+            }
+        }
+    }
+
+    //let mat = mat.into_iter().map(|v| String::from_utf8(v).unwrap()).collect::<Vec<_>>();
+    //dp.iter().for_each(|r| println!("{:?}", r));
+
+    dp[0][idx]
+}
