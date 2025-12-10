ee77837e04465f605d908c68c139415e45e0483f
Isaac <nisaacdz@gmail.com>
2025-12-10 00:53:57 +0000

    Add solution day 8 problem part B

--- day08-B/rust/src/main.rs
+++ day08-B/rust/src/main.rs
@@ -1,45 +1,80 @@
-use std::{
-    fs::File,
-    io::{BufRead, BufReader, Result},
+use glam::IVec3;
+use itertools::Itertools;
+use nom::{
+    IResult, Parser,
+    bytes::complete::tag,
+    character::complete::{self, line_ending},
+    multi::separated_list1,
 };
 
-fn main() -> Result<()> {
-    let file = File::open("../input2.txt")?;
-    let reader = BufReader::new(file);
-    let mut mat = Vec::new();
-    for line in reader.lines() {
-        let line = line?;
-        mat.push(line.into_bytes());
-    }
+fn main() {
+    let input = include_str!("..\\..\\input.txt");
 
-    let result = solve(mat);
-    println!("Result: {}", result);
+    let result = process(input);
 
-    Ok(())
+    println!("Result is : {result}");
 }
 
-fn solve(mut mat: Vec<Vec<u8>>) -> u64 {
-    let (n, m) = (mat.len(), mat[0].len());
-    let mut dp = vec![vec![0; mat[0].len()]; mat.len()];
+pub fn process(input: &str) -> i64 {
+    let (_, positions) = parse(input).unwrap();
+    let output = groups(positions);
+    output
+}
 
-    let idx = mat[0].iter().position(|&c| c == b'S').unwrap();
-    mat[0][idx] = b'|';
-
-    dp[n - 1].iter_mut().for_each(|v| *v = 1);
-
-    for i in (0..(n - 1)).rev() {
-        for j in 0..m {
-            if mat[i][j] == b'^' {
-                dp[i][j] += if j > 0 { dp[i + 1][j - 1] } else { 0 };
-                dp[i][j] += if j + 1 < m { dp[i + 1][j + 1] } else { 0 };
-            } else {
-                dp[i][j] += dp[i + 1][j];
+fn groups(positions: Vec<IVec3>) -> i64 {
+    let mut connections: Vec<Vec<IVec3>> = vec![];
+    let mut last = None::<(IVec3, IVec3)>;
+    for (a, b, _) in positions
+        .iter()
+        .tuple_combinations()
+        .map(|(a, b)| (a, b, a.as_vec3().distance(b.as_vec3())))
+        .sorted_by(|a, b| a.2.partial_cmp(&b.2).unwrap())
+    {
+        let matches = [
+            connections.iter().position(|v| v.contains(a)),
+            connections.iter().position(|v| v.contains(b)),
+        ];
+        match matches {
+            [None, None] => {
+                let _ = last.insert((*a, *b));
+                connections.push(vec![*a, *b]);
             }
+            [Some(index), None] => {
+                let _ = last.insert((*a, *b));
+                connections[index].push(*b);
+            },
+            [None, Some(index)] => {
+                let _ = last.insert((*a, *b));
+                connections[index].push(*a);
+            }
+            [Some(index_a), Some(index_b)] if index_a != index_b => {
+                let _ = last.insert((*a, *b));
+                let a = connections.remove(index_a.max(index_b));
+                let b = connections.remove(index_b.min(index_a));
+                let new_cluster = a
+                    .into_iter()
+                    .chain(b.into_iter())
+                    .unique()
+                    .collect::<Vec<IVec3>>();
+                connections.push(new_cluster);
+            }
+            [Some(_), Some(_)] => {}
+        }
+
+        if connections.len() == 1 && connections[0].len() == positions.len() {
+            println!("Terminating due to condition");
+            break;
         }
     }
+    connections.sort_by(|a, b| b.len().cmp(&a.len()));
 
-    //let mat = mat.into_iter().map(|v| String::from_utf8(v).unwrap()).collect::<Vec<_>>();
-    //dp.iter().for_each(|r| println!("{:?}", r));
+    last.map(|(u, v)| u.x as i64 * v.x as i64).unwrap()
+}
 
-    dp[0][idx]
+fn parse(input: &str) -> IResult<&str, Vec<IVec3>> {
+    separated_list1(
+        line_ending,
+        separated_list1(tag(","), complete::i32).map(|v| IVec3::from_slice(&v)),
+    )
+    .parse(input)
 }
